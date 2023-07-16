2ed18a0e9dcbf1b85fff48854a2ccf09c518ed08
MMyeong <ilikerunning@naver.com>
2023-07-16 23:13:06 +0900

    build: set swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('qfeed API')
+    .setDescription('qfeed staging API description')
+    .setVersion('1.0')
+    .addTag('account')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
+
+
+
