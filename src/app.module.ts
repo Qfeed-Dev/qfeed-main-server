18ea64cffe8209c025a5b0b8665644f516e196fa
MMyeong <ilikerunning@naver.com>
2023-07-22 16:22:19 +0900

    build: set typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [AccountModule],
+  imports: [
+    TypeOrmModule.forRoot(TypeORMConfig),
+    AccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
