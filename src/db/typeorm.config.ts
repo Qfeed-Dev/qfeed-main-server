1ab232e9db4a6159fded98357ee15b61170bf133
MMyeong <ilikerunning@naver.com>
2023-07-25 02:52:37 +0900

    hotfix: shoot db ssl issue

--- src/db/typeorm.config.ts
+++ src/db/typeorm.config.ts
@@ -8,6 +8,14 @@ export const TypeORMConfig: TypeOrmModuleOptions = {
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    ssl: false, // Use SSL/TLS for the connection
+    extra: {
+        ssl: {
+        rejectUnauthorized: false, // Set this to true if you have the proper CA certificate.
+        // If you have the CA certificate, provide it as a buffer:
+        // ca: fs.readFileSync('path_to_your_ca_file.pem').toString(),
+        },
+    },
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true // TODO: 추후 env 로 관리
 }
