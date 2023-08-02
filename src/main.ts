d53472008802c38208d358010220f6f86a803369
MMyeong <ilikerunning@naver.com>
2023-08-02 21:19:45 +0900

    chore: fix enableCors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({credentials: true});
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const swaggerConfig = new DocumentBuilder()
     .setTitle('qfeed API')
