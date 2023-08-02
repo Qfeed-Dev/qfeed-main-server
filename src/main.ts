1a77f491bf1083a10df44a076aef700bf0d6a4e4
MMyeong <ilikerunning@naver.com>
2023-08-02 20:52:53 +0900

    chore: credential 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({credentials: false});
   app.useGlobalPipes(new ValidationPipe());
   const swaggerConfig = new DocumentBuilder()
     .setTitle('qfeed API')
