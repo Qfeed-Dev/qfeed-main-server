e474effcc3fd6a8e3c45cb791adde33a435ae175
MMyeong <ilikerunning@naver.com>
2023-08-05 01:14:35 +0900

    chore: ping api 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/ping')
-  getHello(): string {
+  getHello(): { [key: string]: string } {
     return this.appService.ping();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  ping(): string {
-    return 'pong';
+  ping(): { [key: string]: string } {
+    return { 'message' : 'pong' };
   }
 }
