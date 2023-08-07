2d25ca666f6525c19fe05c5a3069f5bd1ffa6306
MMyeong <82504981+myeongjunkim@users.noreply.github.com>
2023-08-07 12:48:51 +0900

    fix: allowedHeaders 수정

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     origin: true,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe());
   const swaggerConfig = new DocumentBuilder()
