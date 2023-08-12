9b2c51ffaa5166326b933e6eab985cf80192a24f
MMyeong <82504981+myeongjunkim@users.noreply.github.com>
2023-08-12 16:40:07 +0900

    feat: presigned-url API 추가 (#15)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/ping')
-  getHello(): { [key: string]: string } {
+  ping(): { [key: string]: string } {
     return this.appService.ping();
+  }
+
+  @ApiBody({ 
+    schema: {
+      type: 'object',
+      properties: {
+        appName: { type: 'string' },
+        fileName: { type: 'string' },
+        fileType: { type: 'string' }
+      }
+    }
+  })
+  @Post('file/presigned-url')
+  async createPresignedUrl(
+    @Body('appName') appName: string,
+    @Body('fileName') fileName: string, 
+    @Body('fileType') fileType: string
+  ) {
+    return await this.appService.createPresignedUrl(appName, fileName, fileType);
   }
 }
