9f029e0875d0c7b9168841bc838e4e3c4809a1cf
MMyeong <ilikerunning@naver.com>
2023-08-15 14:05:30 +0900

    feat: question app 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AccountModule } from './account/account.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeORMConfig } from './db/typeorm.config';
 import { FileModule } from './file/file.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(TypeORMConfig),
     AccountModule,
     FileModule,
+    QuestionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
