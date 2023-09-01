aa569c7e537d09b1e7c18327b0e87f468748e8a0
MMyeong <ilikerunning@naver.com>
2023-09-02 03:50:56 +0900

    feat: Chatroom API 구현

--- src/chatroom/chatroom.module.ts
+++ src/chatroom/chatroom.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ChatroomController } from './chatroom.controller';
 import { ChatroomService } from './chatroom.service';
+import { TypeOrmExModule } from 'src/db/typeorm-ex.module';
+import { ChatroomRepository, ChatRepository } from './chatroom.repository';
 
 @Module({
+  imports: [
+    TypeOrmExModule.forCustomRepository([
+      ChatroomRepository,
+      ChatRepository,
+    ]),
+  ],
   controllers: [ChatroomController],
   providers: [ChatroomService]
 })
